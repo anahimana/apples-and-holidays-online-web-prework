2fc0ae85b70e87c23eb45c7c3c0a010e03665069
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 22:44:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |key, value|
     puts key.to_s.capitalize! + ":"
+    value.keys
     puts "  Christmas: Lights, Wreath"
     binding.pry
   end
