1df3f18167e83ef98d65c39ad8e139575f0bfcc3
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 21:19:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, season|
+  holiday_hash.each do |season, holidays|
     season = key.to_s.capitalize + ":"
     season
     binding.pry
