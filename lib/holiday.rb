3a8f614f3c56c62ed639ffffd287857d79744167
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 21:12:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-    holiday_hash.each do |key, value|
-      key
-      value
+  holiday_hash.each do |key, value|
+    key
+    value
     end
   end
 
