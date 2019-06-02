fc86a39d12c392701ac9ac0b82fca2ff6ef3a080
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 20:33:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.collect {|key, value| key == "winter"}
+  binding.pry
 
 end
 
