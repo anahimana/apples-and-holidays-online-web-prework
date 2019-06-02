126b89e5f5fff4c09e63ec392e7db053b2914b07
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 21:10:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |key, value|
   end
+  end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
