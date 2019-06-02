24e315492eefb023559523542848251395388712
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 22:01:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   my_str = ""
   holiday_hash.each do |key, value|
     my_str << key.to_s.capitalize + ":\n"
-    binding.pry
+    value.each do |key, value|
   end
 end
 
