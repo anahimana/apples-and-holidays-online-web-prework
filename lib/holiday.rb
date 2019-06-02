148ae6b432446bf9e25eedfc409b1e345ddad697
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 23:12:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |key, value|
-    valu.each do |key, value|
+    value.each do |key, value|
       binding.pry
     end
   end
