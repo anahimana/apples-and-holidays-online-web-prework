b6ba004dd045f477a61af38706f3698dacbec24f
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-02 21:58:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts key
     puts value
-  end
   binding.pry
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
